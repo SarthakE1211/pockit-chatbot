f9196442db4a3c314f87baeaa56991e1da3bdeee
Sarthak Ekhande <skekhande@outlook.com>
2026-03-25 17:45:27 +0530

    fix iframe CSP in main app

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src *; frame-src *; frame-ancestors *; img-src * data: blob:; script-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
